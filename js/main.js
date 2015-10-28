7685af4f26c6ef9f7de5d42eba4209b2f738d19f
jdalby81 <jdalby81@gmail.com>
2015-10-28 17:03:45 -0400

    js
    javascript

---
+++ js/main.js
@@ -0,0 +1,190 @@
+$(document).ready(function(){
+//This is controlling the form audio visual Form A
+//this will reset the values in the form checkbox and
+//ensure the  values coming back to the admin are easier to read
+//----------19" LCD FLAT SCREEN MONITOR
+$(".rsform-block-19inchlcdflatvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('19inchLcdFlatQuantity').value='1';
+    });
+$(".rsform-block-19inchlcdflatvalue input:nth-of-type(1)").click(function(){
+document.getElementById('19inchLcdFlatQuantity').value='0';
+    });
+//----------22" LCD FLAT SCREEN MONITOR
+$(".rsform-block-22inchlcdflatvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('22inchLcdFlatQuantity').value='1';
+    });
+$(".rsform-block-22inchlcdflatvalue input:nth-of-type(1)").click(function(){
+document.getElementById('22inchLcdFlatQuantity').value='0';
+    });
+//----------32" LCD FLAT SCREEN MONITOR
+$(".rsform-block-32inchflatvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('32InchFlatQuantity').value='1';
+    });
+$(".rsform-block-32inchlcdflatvalue input:nth-of-type(1)").click(function(){
+document.getElementById('32InchFlatQuantity').value='0';
+    });
+//----------42" PLASMA MONITOR WITH SPEAKERS
+$(".rsform-block-42inchplasmavalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('42InchPlasmaQuantity').value='1';
+    });
+$(".rsform-block-42inchplasmavalue input:nth-of-type(1)").click(function(){
+document.getElementById('42InchPlasmaQuantity').value='0';
+    });
+//----------50" PLASMA MONITOR WITH SPEAKERS
+$(".rsform-block-50inchplasmawithspeakersvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('50InchPlasmaWithSpeakersQuantity').value='1';
+    });
+$(".rsform-block-50inchplasmawithspeakersvalue input:nth-of-type(1)").click(function(){
+document.getElementById('50InchPlasmaWithSpeakersQuantity').value='0';
+    });
+//----------VHS WITH AUTO REPEAT
+$(".rsform-block-vhsautorepeatvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('vhsAutoRepeatQuantity').value='1';
+    });
+$(".rsform-block-vhsautorepeatvalue input:nth-of-type(1)").click(function(){
+document.getElementById('vhsAutoRepeatQuantity').value='0';
+    });
+//----------DVD PLAYER
+$(".rsform-block-dvdplayervalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('dvdPlayerQuantity').value='1';
+    });
+$(".rsform-block-dvdplayervalue input:nth-of-type(1)").click(function(){
+document.getElementById('dvdPlayerQuantity').value='0';
+    });
+//----------54" AV CART W/ BLACK SKIRT
+$(".rsform-block-54inchavcartvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('54InchAvCartQuantity').value='1';
+    });
+$(".rsform-block-54inchavcartvalue input:nth-of-type(1)").click(function(){
+document.getElementById('54InchAvCartQuantity').value='0';
+    });
+//----------27" TV
+$(".rsform-block-27inchtvvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('27InchTvQuantity').value='1';
+    });
+$(".rsform-block-27inchtvvalue input:nth-of-type(1)").click(function(){
+document.getElementById('27InchTvQuantity').value='0';
+    });
+//----------CD PLAYER ONLY - SINGLE DISC
+$(".rsform-block-cdplayervalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('cdPlayerQuantity').value='1';
+    });
+$(".rsform-block-cdplayervalue input:nth-of-type(1)").click(function(){
+document.getElementById('cdPlayerQuantity').value='0';
+    });
+//----------CASSETTE DECK
+$(".rsform-block-cassettedeckvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('cassetteDeckQuantity').value='1';
+    });
+$(".rsform-block-cassettedeckvalue input:nth-of-type(1)").click(function(){
+document.getElementById('cassetteDeckQuantity').value='0';
+    });
+//----------SMALL BOOTH AUDIO SYSTEM - WIRED MIC 
+$(".rsform-block-smallboothwiredvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('smallBoothWiredQuantity').value='1';
+    });
+$(".rsform-block-smallboothwiredvalue input:nth-of-type(1)").click(function(){
+document.getElementById('smallBoothWiredQuantity').value='0';
+    });
+//----------SMALL BOOTH AUDIO SYSTEM - WIRELESS MIC  
+$(".rsform-block-smallboothwirelessvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('smallBoothWirelessQuantity').value='1';
+    });
+$(".rsform-block-smallboothwirelessvalue input:nth-of-type(1)").click(function(){
+document.getElementById('smallBoothWirelessQuantity').value='0';
+    });
+//----------IBM DESKTOP PACKAGE  
+$(".rsform-block-ibmdesktopvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('ibmDesktopQuantity').value='1';
+    });
+$(".rsform-block-ibmdesktopvalue input:nth-of-type(1)").click(function(){
+document.getElementById('ibmDesktopQuantity').value='0';
+    });
+//----------IBM LAPTOP  
+$(".rsform-block-ibmlaptopvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('ibmLaptopQuantity').value='1';
+    });
+$(".rsform-block-ibmlaptopvalue input:nth-of-type(1)").click(function(){
+document.getElementById('ibmLaptopQuantity').value='0';
+    });
+//----------MAC NOTEBOOK 
+$(".rsform-block-macnotebookvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('macNotebookQuantity').value='1';
+    });
+$(".rsform-block-macnotebookvalue input:nth-of-type(1)").click(function(){
+document.getElementById('macNotebookQuantity').value='0';
+    });
+//----------PLASMA FLOOR MOUNT 
+$(".rsform-block-plasmafloorvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('plasmaFloorQuantity').value='1';
+    });
+$(".rsform-block-plasmafloorvalue input:nth-of-type(1)").click(function(){
+document.getElementById('plasmaFloorQuantity').value='0';
+    });
+//----------LCD FLOOR MOUNT 
+$(".rsform-block-lcdfloorvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('lcdFloorQuantity').value='1';
+    });
+$(".rsform-block-lcdfloorvalue input:nth-of-type(1)").click(function(){
+document.getElementById('lcdFloorQuantity').value='0';
+    });
+//----------PLASMA/LCD WALL MOUNTS 
+$(".rsform-block-plasmalcdwallvalue input:nth-of-type(2), .formField input:nth-of-type(3)", this).click(function(){
+document.getElementById('plasmaLcdWallQuantity').value='1';
+    });
+$(".rsform-block-plasmalcdwallvalue input:nth-of-type(1)").click(function(){
+document.getElementById('plasmaLcdWallQuantity').value='0';
+    });
+
+
+//handling about page toolbox toggle
+
+$('.tool').click(function(){
+    $('.toolboxList').toggle();
+});
+});
+//handling contact form
+/*
+Creating an HTML5 enhanced responsive-ready contact form, with custom javascript feature detection
+www.toddmotto.com
+*/
+/*(function() {
+
+	// Create input element for testing
+	var inputs = document.createElement('input');
+	
+	// Create the supports object
+	var supports = {};
+	
+	supports.autofocus   = 'autofocus' in inputs;
+	supports.required    = 'required' in inputs;
+	supports.placeholder = 'placeholder' in inputs;
+
+	// Fallback for autofocus attribute
+	if(!supports.autofocus) {
+		
+	}
+	
+	// Fallback for required attribute
+	if(!supports.required) {
+		
+	}
+
+	// Fallback for placeholder attribute
+	if(!supports.placeholder) {
+		
+	}
+	
+	// Change text inside send button on submit
+	var send = document.getElementById('contact-submit');
+	if(send) {
+		send.onclick = function () {
+			this.innerHTML = '...Sending';
+		}
+	}
+
+})();*/
+
+
+
+   
